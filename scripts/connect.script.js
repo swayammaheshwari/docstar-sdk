c3a871aece83343a6a67828bfbb0cfbf686f199b
Swayam Maheshwari <swayammaheshwari890@gmail.com>
2025-09-04 18:04:22 +0530

    chore: update connection success log message text

--- scripts/connect.script.js
+++ scripts/connect.script.js
@@ -3,7 +3,7 @@
 
 (function () {
     // Log connection success message
-    console.log('connected successfully new');
+    console.log('connected successfully new one');
 
     // Optional: You can also add additional initialization logic here
     // For example, setting up global variables or event listeners
